cec1d8e1b404193efa0e2ff017eb26ec0c2d04ca
sir hillary <h.o.omondi15@gmail.com>
2025-08-02 08:58:03 +0300

    backend fix commit

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+
 
 const userSchema = new mongoose.Schema({
-    _id:{type: String, required: true},
+    _id: {type: String, required: true},
     name: {type: String, required: true},
-    email: {type: String, required: true},
-    image: {type: String, required: true}
-})
+    email: {type: String, required: true, unique: true},
+    image: {type: String, required: true},
+}, { timestamps: true });
 
-const User = mongoose.model('User', userSchema);
+export default mongoose.model('User', userSchema);
+// export default mongoose.model('User', userSchema);
 
-export default User;
