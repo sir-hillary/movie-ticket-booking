ee8f80398c6f1ab2e3e29f70bcfa77e3d75feb57
sir hillary <h.o.omondi15@gmail.com>
2025-08-19 09:19:08 +0300

    another commit on backend bug

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://sirhillary:sirhillary15@cluster0.rn44mg1.mongodb.net/`)
+        await mongoose.connect(`mongodb+srv://sirhillary:sirhillary15@cluster0.rn44mg1.mongodb.net/myDatabase?retryWrites=true&w=majority`)
         console.log('MongoDB connected successfully!');
     } catch (error) {
         console.error("Database connection failed!", error);
